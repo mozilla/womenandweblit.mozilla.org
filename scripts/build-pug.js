dd26babe7d02d120024660ec9b6a865ee9c9578d
Gavin Lazar Suntop <gavin@gsuntop.com>
2016-08-10 12:11:21 -0700

    WWL initial scaffolding & architecture

---
+++ scripts/build-pug.js
@@ -0,0 +1,8 @@
+let pug = require(`pug`);
+let shelljs = require(`shelljs`);
+
+let compiled = pug.renderFile(`source/index.pug`, {
+  pretty: true
+});
+
+shelljs.ShellString(compiled).to(`dest/index.html`);
